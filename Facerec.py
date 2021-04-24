06425c55501465c7242140984123523e7aec6794
Fabian Luettel <fabianluettel@web.de>
2021-04-24 11:23:33 +0200

    face detection for video

--- Facerec.py
+++ Facerec.py
@@ -17,13 +17,14 @@ from torchvision import datasets
 import numpy as np
 import pandas as pd
 import os
+import cv2 as cv
 
 #own python stuff
 from dataconverter import convert_absolute_to_relative, convert_relative_to_class
 
 #pick mode here
-#mode = "livevideo"
-mode = "singleimages"
+mode = "livevideo"
+#mode = "singleimages"
 
 
 workers = 0 if os.name == 'nt' else 4
@@ -62,61 +63,97 @@ if mode == "singleimages":
     dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
     loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
 
-#elif mode == "livevideo":
-    #
+elif mode == "livevideo":
+    cap = cv.VideoCapture(0)  #0 = read webcam
+    counter = 0
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        counter = counter + 1;
+        if ((counter%200)==0):
+            print(counter)
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            # if frame is read correctly ret is True
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+            # Our operations on the frame come here
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            # Display the resulting frame
+            cv.imshow('frame', gray)
+            if cv.waitKey(1) == ord('q'):
+                break
+
+            #face detection here for each frame:
+            x_aligned, prob = mtcnn(frame, return_prob=True) #todo is frame das richtige argument?
+            if x_aligned is not None:
+                print('Face detected with probability: {:8f}'.format(prob))
+
+            #todo draw bounding boxes,
+            # todo workers etc.
 
 
-#Perfom MTCNN facial detection
-#Iterate through the DataLoader object and detect faces and associated detection probabilities for each. The MTCNN forward method returns images cropped to the detected face, if a face was detected. By default only a single detected face is returned - to have MTCNN return all detected faces, set keep_all=True when creating the MTCNN object above.
-#To obtain bounding boxes rather than cropped face images, you can instead call the lower-level mtcnn.detect() function. See help(mtcnn.detect) for details.
-aligned = []
-names = []
-for x, y in loader:
-    x_aligned, prob = mtcnn(x, return_prob=True)
-    if x_aligned is not None:
-        print('Face detected with probability: {:8f}'.format(prob))
-        aligned.append(x_aligned)
-        names.append(dataset.idx_to_class[y])
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
 
-#Calculate image embeddings
-#MTCNN will return images of faces all the same size,
-# enabling easy batch processing with the Resnet
-# recognition module. Here, since we only have a few
-# images, we build a single batch and perform inference on it.
-#For real datasets, code should be modified to control batch sizes
-# being passed to the Resnet, particularly if being processed on a GPU.
-# For repeated testing, it is best to separate face detection (using MTCNN)
-# from embedding or classification (using InceptionResnetV1), as calculation of cropped faces or bounding boxes
-# can then be performed a single time and detected faces saved for future use.
-aligned = torch.stack(aligned).to(device)
-embeddings = resnet(aligned).detach().cpu()
 
-#Print distance matrix for classes
-dists = [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
-#Debugger:
-#dists = [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
-#dists = np.array(dists)
-#dists
-#formt ein numpyarray der Ergebnisse
 
-df = pd.DataFrame(dists, columns=names, index=names)
-print(df)
 
-df_relative = df.applymap(convert_absolute_to_relative)
-print(df_relative)
+if mode == "singleimages":
+    #Perfom MTCNN facial detection
+    #Iterate through the DataLoader object and detect faces and associated detection probabilities for each. The MTCNN forward method returns images cropped to the detected face, if a face was detected. By default only a single detected face is returned - to have MTCNN return all detected faces, set keep_all=True when creating the MTCNN object above.
+    #To obtain bounding boxes rather than cropped face images, you can instead call the lower-level mtcnn.detect() function. See help(mtcnn.detect) for details.
+    aligned = []
+    names = []
+    for x, y in loader:
+        x_aligned, prob = mtcnn(x, return_prob=True)
+        if x_aligned is not None:
+            print('Face detected with probability: {:8f}'.format(prob))
+            aligned.append(x_aligned)
+            names.append(dataset.idx_to_class[y])
 
-df_message = df_relative.applymap(convert_relative_to_class)
-print(df_message)
+    #Calculate image embeddings
+    #MTCNN will return images of faces all the same size,
+    # enabling easy batch processing with the Resnet
+    # recognition module. Here, since we only have a few
+    # images, we build a single batch and perform inference on it.
+    #For real datasets, code should be modified to control batch sizes
+    # being passed to the Resnet, particularly if being processed on a GPU.
+    # For repeated testing, it is best to separate face detection (using MTCNN)
+    # from embedding or classification (using InceptionResnetV1), as calculation of cropped faces or bounding boxes
+    # can then be performed a single time and detected faces saved for future use.
+    aligned = torch.stack(aligned).to(device)
+    embeddings = resnet(aligned).detach().cpu()
 
-#another conversion function for converting the relative numbers into similarity values:
+    #Print distance matrix for classes
+    dists = [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
+    #Debugger:
+    #dists = [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
+    #dists = np.array(dists)
+    #dists
+    #formt ein numpyarray der Ergebnisse
 
-unique_names = list(dataset.class_to_idx.keys())
+    df = pd.DataFrame(dists, columns=names, index=names)
+    print(df)
 
-# best_match = {}
-# for row in df
-    # for un in unique_names
-        # select all columns from df where column name equals un
-        # sel_entry = ...
-        # val = sel_entry[sel_entry != 0].min()
-        # best_match[row_name][un] = val  
+    df_relative = df.applymap(convert_absolute_to_relative)
+    print(df_relative)
+
+    df_message = df_relative.applymap(convert_relative_to_class)
+    print(df_message)
+
+    #another conversion function for converting the relative numbers into similarity values:
+
+    unique_names = list(dataset.class_to_idx.keys())
+
+    # best_match = {}
+    # for row in df
+        # for un in unique_names
+            # select all columns from df where column name equals un
+            # sel_entry = ...
+            # val = sel_entry[sel_entry != 0].min()
+            # best_match[row_name][un] = val
 
